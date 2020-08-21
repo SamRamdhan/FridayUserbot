7516f4597c575d48c75c259248d0860aaa08b56b
SamRamdhan <69948351+SamRamdhan@users.noreply.github.com>
2020-08-21 18:49:09 +0800

    Update remove.bg.py

--- userbot/plugins/remove.bg.py
+++ userbot/plugins/remove.bg.py
@@ -47,7 +47,7 @@ async def _(event):
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "@FridayOT.png"
+            remove_bg_image.name = "canzu.png"
             await borg.send_file(
                 event.chat_id,
                 remove_bg_image,
@@ -58,7 +58,7 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit("Removed dat annoying Backgroup in {} seconds, powered by Friday Userbot".format(ms))
+        await event.edit("Background telah dihapus dalam waktu {} detik, powered by canzu si gabut".format(ms))
     else:
         await event.edit("ReMove.BG API returned Errors. Please report to @FridayOt Support Group\n`{}".format(output_file_name.content.decode("UTF-8")))
 
