fc8a58be3aba789e7aa98e11429feb9a6c3e968d
SamRamdhan <69948351+SamRamdhan@users.noreply.github.com>
2020-09-12 01:34:02 +0800

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,18 +7,18 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/8ad91e47be4fb6027d858.jpg"
-pm_caption = "`USERBOT :` **AKTIF**\n\n"
-pm_caption += "**PROFIL PENGGUNA**\n"
-pm_caption += "**NAMA** : `SAM RAMADHAN`\n`a.k.a :` **SAM**\n"
-pm_caption += "**JENIS KELAMIN** : `LAKI-LAKI`\n"
+pm_caption = "`STATUS USERBOT :` __AKTIF__ \n\n"
+pm_caption += "**PROFIL PENGGUNA**\n\n"
+pm_caption += "**NAMA** : `SAM RAMADHAN`\n"
+pm_caption += f"**USERNAME** : {DEFAULTUSER} \n"
 pm_caption += "**DOMISILI** : `MAKASSAR, INDONESIA`\n"
 pm_caption += "**UMUR** : `GAADA YANG TAU`\n"
 pm_caption += "**HOBBY** : `IBADAH`\n"
-pm_caption += f"**USERNAME** : {DEFAULTUSER} \n"
-pm_caption += "**GROUP SAYA** : @KerabatOnline\n\n"
-pm_caption += "• [Kerabat Online Twitter](https://twitter.com/KerabatOnline)\n"
-pm_caption += "• [Kerabat Online Channel](https://t.me/KerabatOnline_Ch)\n"
-pm_caption += "• [Kerabat Moment](https://t.me/KerabatMoment)"
+pm_caption += "**JENIS KELAMIN** : `LAKI-LAKI` \n\n"
+pm_caption += "**Founder Group** @KerabatOnline\n"
+pm_caption += "🌐 [Kerabat Online Twitter](https://twitter.com/KerabatOnline)\n"
+pm_caption += "📣 [Kerabat Online Channel](https://t.me/KerabatOnline_Ch)\n"
+pm_caption += "📝 [Kerabat Online Moment](https://t.me/KerabatMoment)"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
