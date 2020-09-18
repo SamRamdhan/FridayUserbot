6609882e88e6a9db12180eeff1239c04425a5fdf
SamRamdhan <69948351+SamRamdhan@users.noreply.github.com>
2020-09-18 12:52:32 +0800

    Update quotly.py

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -13,18 +13,18 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.edit("```Reply pesan untuk dijadikan stiker.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("```Reply to text message```")
+       await event.edit("```Reply pesan untuk dijadikan stike```")
        return
     chat = "@QuotLyBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await event.edit("```Reply pengguna aktif.```")
        return
-    await event.edit("```Making a Quote```")
+    await event.edit("```Membuat stiker quotly```")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
